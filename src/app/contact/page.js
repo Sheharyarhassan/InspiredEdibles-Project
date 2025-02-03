9511b50fd12cfd5f2212c5f1d095981f1b801644
hassan-techtonex <hassan@techtonex.com>
2025-02-03 16:10:00 +0500

    Changes

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -7,7 +7,7 @@ export default function ContactUs() {
     <>
       <Container fluid >
         <Row className="justify-content-between">
-            <Col sm={3}>
+            <Col xl={3} lg={4} md={5}>
             <p className="fw-lighter mt-4">/ CONTACT</p>
             <h3 className="fw-light mt-4">We love working with on everything tell us your story</h3>
             <button className="w-100 btn btn-outline-dark rounded-pill mt-5">Connect with us!</button>
@@ -15,7 +15,7 @@ export default function ContactUs() {
             
             <Image src={donut} className="col-4 mw-100 h-auto position-fixed bottom-0 start-50 translate-middle-x"/>
             
-            <Col sm={3}>
+            <Col xl={4} lg={4} md={5}>
             <p className="fw-lighter mt-4">/ SOCIAL</p>
             <div className="d-flex">
               <button className="btn btn-outline-dark  rounded-pill-dotted">YOUTUBE</button>
